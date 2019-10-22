0a11c192c6c3fa8eea0d4bd37b532ec11c897508
isimplyexcel <isimplyexcel@gmail.com>
2019-10-21 21:35:16 -0700

    finished basic user stories for flix

---
+++ flixter/flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Paul Min on 10/21/19.
+//  Copyright © 2019 Paul Min. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
